8a720bd3447a496a6ab92f6988fd9d1db34d5b93
Sujal027 <148904402+Sujal027@users.noreply.github.com>
2024-07-12 22:11:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+let ys = document.querySelector("#yourScore").innerText;
+let cs = document.querySelector("#computerScore").innerText;
+ys = Number(ys);
+cs = Number(cs);
+document.querySelector("#scissors").addEventListener("click", ()=>{
+    let e =document.querySelector("#scissors");
+   let n = Math.random()*10;
+    if( n >=0 && n <=3)
+    {
+      c = "scissors";   
+    }
+    else if( n > 3 && n <=7)
+    {
+        c = "rock";
+    }
+    else{
+        c = "paper";
+    }
+ 
+    let e1 = document.querySelector("#currentStatus");
+    if(c == "scissors")
+    {
+     e1.innerText = "you chose scissors and computer also chose scissors";
+    }
+    else if(c == "rock")
+    {
+        e1.innerText="you chose scissors and computer chose rock";
+
+      cs++;
+      document.querySelector("#computerScore").innerText = cs;
+
+    }
+else{
+    e1.innerText= "you chose scissors and computer chose papers";
+    ys++;
+     document.querySelector("#yourScore").innerText = ys;
+}
+})
+document.querySelector("#rock").addEventListener("click", ()=>{
+    let e =document.querySelector("#scissors");
+   let n = Math.random()*10;
+    if( n >=0 && n <=3)
+    {
+      c = "scissors";   
+    }
+    else if( n > 3 && n <=7)
+    {
+        c = "rock";
+    }
+    else{
+        c = "paper";
+    }
+ 
+    let e1 = document.querySelector("#currentStatus");
+    if(c == "rock")
+    {
+     e1.innerText = "you chose rock and computer also chose rock";
+    }
+    else if(c == "paper")
+    {
+        e1.innerText="you chose rock and computer chose paper";
+
+      cs++;
+      document.querySelector("#computerScore").innerText = cs;
+
+    }
+else{
+    e1.innerText= "you chose rock and computer chose scissors";
+    ys++;
+     document.querySelector("#yourScore").innerText = ys;
+}
+})
+document.querySelector("#paper").addEventListener("click", ()=>{
+    let e =document.querySelector("#scissors");
+   let n = Math.random()*10;
+    if( n >=0 && n <=3)
+    {
+      c = "scissors";   
+    }
+    else if( n > 3 && n <=7)
+    {
+        c = "rock";
+    }
+    else{
+        c = "paper";
+    }
+ 
+    let e1 = document.querySelector("#currentStatus");
+    if(c == "paper")
+    {
+     e1.innerText = "you chose paper and computer also chose paper";
+    }
+    else if(c == "scissors")
+    {
+        e1.innerText="you chose paper and computer chose scissors";
+
+      cs++;
+      document.querySelector("#computerScore").innerText = cs;
+
+    }
+else{
+    e1.innerText= "you chose paper and computer chose rock";
+    ys++;
+     document.querySelector("#yourScore").innerText = ys;
+}
+})
+document.querySelector("#reset").addEventListener("click" , () => {
+    document.querySelector("#yourScore").innerText = 0;
+    document.querySelector("#computerScore").innerText = 0;
+    document.querySelector("#currentStatus").innerText = "";
+
+})
